460b130190ad1d69f98f498027702834e89bf3cc
Malcolm Peterson <mgpetersn@gmail.com>
2021-08-09 23:14:54 -0400

    added form structure to component
    built forms for adding, updating and deleting employees in database.
    next: work in form submission

--- frontend/src/register.js
+++ frontend/src/register.js
@@ -1,8 +1,36 @@
 const Register = () => {
   return (
     <div>
-      <h1>Register</h1>
-      <form></form>
+      <h1>Forms</h1>
+
+      <form className="create_form">
+        <h3>create employee record</h3>
+        <input type="text" placeholder="Full Name"></input>
+        <input type="text" placeholder="Department"></input>
+        <input type="text" placeholder="Job Title"></input>
+        <input type="text" placeholder="Email Address"></input>
+        <button className="create_button">Submit</button>
+      </form>
+
+      <form className="update_form">
+        <h3>update employee record</h3>
+        <input type="text" placeholder="ID Number"></input>
+        <select name="options" className="dropdown_list">
+          <option value="">--Choose Option--</option>
+          <option value="department">Department</option>
+          <option value="email">Email</option>
+          <option value="job">Job Title</option>
+          <option value="name">Full Name</option>
+        </select>
+        <input type="text" placeholder="updated selection"></input>
+        <button className="update_button">Submit</button>
+      </form>
+
+      <form className="delete_form">
+        <h3>delete employee record</h3>
+        <input type="text" placeholder="Employee ID"></input>
+        <button className="delete_button">Delete</button>
+      </form>
     </div>
   );
 };
