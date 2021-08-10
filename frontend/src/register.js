6d25451dda3a20d3fc7a47d6ffe4c0728d8acf40
Malcolm Peterson <mgpetersn@gmail.com>
2021-08-10 01:40:06 -0400

    rewrote forms
    reworked forms and implemented /installed react-hook-form to assist with getting data from the inputs.

--- frontend/src/register.js
+++ frontend/src/register.js
@@ -1,38 +1,52 @@
-const Register = () => {
-  return (
+import React from "react";
+import {useForm} from "react-hook-form";
+
+export default function Register() {
+
+  const {register, handleSubmit} = useForm();
+
+  //create employee (post)
+  const submitPost =(data)=>{
+    console.log(data)
+  }
+  //update employee (patch)
+  const submitPatch =(data)=>{
+    console.log(data)
+  }
+  //delete employee (delete)
+  const submitDelete =(data)=>{
+    console.log(data.id)
+  }
+  return(
     <div>
-      <h1>Forms</h1>
+    <form onSubmit={handleSubmit(submitPost)}>
+      <h3>create employee record</h3>
+      <input type="text" placeholder="Full Name" {...register('name')}/>
+      <input type="text" placeholder="Department" {...register('department')}/>
+      <input type="text" placeholder="Job Title" {...register('job_title')}/>
+      <input type="text" placeholder="Email Address" {...register('email')}/>
+      <button type="submit">Submit</button>
+     </form>
 
-      <form className="create_form">
-        <h3>create employee record</h3>
-        <input type="text" placeholder="Full Name"></input>
-        <input type="text" placeholder="Department"></input>
-        <input type="text" placeholder="Job Title"></input>
-        <input type="text" placeholder="Email Address"></input>
-        <button className="create_button">Submit</button>
-      </form>
+    <form onSubmit={handleSubmit(submitPatch)}className="update_form">
+      <h3>update employee record</h3>
+      <input type="text" placeholder="ID Number" {...register('update_id')}/>
+      <select name="options" className="dropdown_list" {...register('key')}>
+        <option value="">--Choose Option--</option>
+        <option value="department">Department</option>
+        <option value="email">Email</option>
+        <option value="job">Job Title</option>
+        <option value="name">Full Name</option>
+      </select>
+      <input type="text" placeholder="Update" {...register('value')}></input>
+      <button type="submit">Submit</button>
+    </form>
 
-      <form className="update_form">
-        <h3>update employee record</h3>
-        <input type="text" placeholder="ID Number"></input>
-        <select name="options" className="dropdown_list">
-          <option value="">--Choose Option--</option>
-          <option value="department">Department</option>
-          <option value="email">Email</option>
-          <option value="job">Job Title</option>
-          <option value="name">Full Name</option>
-        </select>
-        <input type="text" placeholder="updated selection"></input>
-        <button className="update_button">Submit</button>
-      </form>
-
-      <form className="delete_form">
-        <h3>delete employee record</h3>
-        <input type="text" placeholder="Employee ID"></input>
-        <button className="delete_button">Delete</button>
-      </form>
-    </div>
-  );
-};
-
-export default Register;
+    <form onSubmit={handleSubmit(submitDelete)}>
+      <h3>delete employee record</h3>
+      <input type="text" placeholder="ID Number" {...register('id')}/>
+      <button type="submit">Delete</button>
+    </form>
+</div>
+  )
+}
