f683291e2f63903dd9252f3ea73be7ab47a70678
Malcolm Peterson <mgpetersn@gmail.com>
2021-08-11 12:43:12 -0400

    cors added to server
    added CORS to server.

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
+const data = require('./models/employee')
+app.use(cors())
 
-
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.ATLAS_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection;
 db.on('error', (error)=> console.error(error));
 db.once('open', ()=> console.log('CONNECTED to database'));
@@ -15,6 +17,8 @@ app.use(express.json())
 
 const mainRouter = require('./routes/employees')
 app.use('/employees', mainRouter)
+
+
 
 
 app.listen(3000, ()=>console.log("Server START"))
