ba2190a158e65fd55b78d692b11951ed5af75865
Malcolm Peterson <mgpetersn@gmail.com>
2021-08-12 01:09:39 -0400

    updated homepage styling
    added and imported images and fonts for homepage styling.

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,9 +1,18 @@
-const Home = () =>{
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+import firstImage from "./stockphotos/ladylaptop.jpeg";
+import "./homepage.css";
 
-export default Home
+const Home = () => {
+  return (
+    <div>
+      <div className="homeSplash">
+        <img src={firstImage} className="frontImage" alt="lady on laptop"></img>
+        <div className="frontText"><span className="homeHeading">Databases have never been so fun.</span> <br />
+        <p>mPloy frees HR to perform the big-picture projects that make a difference. Businesses can streamline tedious database managment processes and improve their employee experience by moving away from paper and spreadsheets and into our intuitive, web-based software.</p>
+        <a href="/forms" className="topButton">Let's Get Started</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
