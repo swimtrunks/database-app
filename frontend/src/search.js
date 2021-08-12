f0212702f451adfe0e04304aed6400017ae898b4
Malcolm Peterson <mgpetersn@gmail.com>
2021-08-12 01:10:13 -0400

    updated navbar styles
    updated navbar styles

---
+++ frontend/src/search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Search = () => {
+  return (
+    <div>
+      <form className="searchbar">
+        <input type="text" />
+        <input type="text" />
+        <button className="searchButton"type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
