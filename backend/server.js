fef7d7a891942e5a83c00a39347decffd7eb2713
Malcolm Peterson <mgpetersn@gmail.com>
2021-08-13 11:08:46 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 
 require('dotenv').config()
+const Employee_data = require('./models/employee')
+
 
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
-const data = require('./models/employee')
+
 app.use(cors())
 
 mongoose.connect(process.env.ATLAS_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
@@ -15,10 +17,31 @@ db.once('open', ()=> console.log('CONNECTED to database'));
 
 app.use(express.json())
 
+
+//routes
+
 const mainRouter = require('./routes/employees')
 app.use('/employees', mainRouter)
 
+//department filter
+app.get('/search/department/:department', findDept, (request, response) =>{
+  
+  response.send(response.deptList)
+})
 
+async function findDept(request, response, next){
 
+  let deptList;
+  try{
+      deptList = await Employee_data.find({department : request.params.department})
+      if(deptList == undefined){
+          return response.status(404).json({message: "no departments found"})
+      }
+  }catch(error){
+      return response.status(500).json({message: error.message})
+  }
+  response.deptList = deptList
+  next()
+}
 
 app.listen(3000, ()=>console.log("Server START"))
